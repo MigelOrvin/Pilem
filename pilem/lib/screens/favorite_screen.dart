161adc6abef377c058870f2671ce72ef24730324
Migel <migel8206@gmail.com>
2025-03-12 08:39:21 +0700

    upd fav screen

--- pilem/lib/screens/favorite_screen.dart
+++ pilem/lib/screens/favorite_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
 import 'package:pilem/screens/detail_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -11,6 +14,31 @@ class FavoriteScreen extends StatefulWidget {
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
   List<Movie> _favoriteMovies = [];
+
+  Future<void> _loadFavoriteMovies() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<String> favoriteMovieIds = prefs.getKeys().where((key) => key.startsWith('movie_')).toList();
+    print('favoriteMovies: $favoriteMovieIds');
+    
+    setState(() {
+      _favoriteMovies = favoriteMovieIds.map((id) {
+        final String? movieJson = prefs.getString(id);
+        if (movieJson != null && movieJson.isNotEmpty) {
+          final Map<String, dynamic> movieData = jsonDecode(movieJson);
+          return Movie.fromJson(movieData);
+        }
+        return null;
+      }).where((movie) => movie != null).cast<Movie>().toList();
+    });
+ 
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadFavoriteMovies();
+  }
 
   @override
   Widget build(BuildContext context) {
